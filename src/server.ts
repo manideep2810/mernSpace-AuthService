d746316a03e7ade7b43ca99fed95bd3733e4b032
Manideep Naidu <manideepnaidugorle@gmail.com>
2025-05-12 00:29:14 +0530

    add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,3 @@
-function f(name: string) {
-    const user = {
-        fname: 'John',
-    }
-    const ffname = user.fname
-    console.log(`Hello ${name} ${ffname}`)
-}
+import { Config } from './config'
 
-f('World')
+console.log(Config.PORT)
