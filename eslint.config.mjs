e13b77e071dfb90620608483b895dfca545b1bf8
Manideep Naidu <manideepnaidugorle@gmail.com>
2025-05-12 23:04:25 +0530

    fix config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,12 @@ export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.recommendedTypeChecked,
     {
-        ignores: ['node_modules', 'dist', 'eslint.config.mjs','jest.config.js'],
+        ignores: [
+            'node_modules',
+            'dist',
+            'eslint.config.mjs',
+            'jest.config.js',
+        ],
     },
     {
         languageOptions: {
