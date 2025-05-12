19b967797c630b89d09210ae59d061418644d79c
Manideep Naidu <manideepnaidugorle@gmail.com>
2025-05-12 23:01:09 +0530

    add test

---
+++ app.spec.ts
@@ -0,0 +1,15 @@
+import { calculateDiscount } from './src/utils'
+import request from 'supertest'
+import app from './src/app'
+
+describe('App', () => {
+    it('should return the correct discount amount', () => {
+        const discount = calculateDiscount(100, 10)
+        expect(discount).toBe(10)
+    })
+
+    it('should return 200 for the root endpoint', async () => {
+        const res = await request(app).get('/').send()
+        expect(res.statusCode).toBe(200)
+    })
+})
