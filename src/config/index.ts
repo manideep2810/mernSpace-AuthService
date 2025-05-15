38d397ac1b7d28ffe2f135fd463fd2de3ea25cf5
Manideep Naidu <manideepnaidugorle@gmail.com>
2025-05-15 19:15:45 +0530

    setup database

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,17 @@
-const PORT = process.env.PORT
-const NODE_ENV = process.env.NODE_ENV
+import path from 'path'
+import { config } from 'dotenv'
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) })
+// console.log(process.env.NODE_ENV);
+
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_NAME, DB_PASSWORD, DB_USERNAME } =
+    process.env
 
 export const Config = {
     PORT,
     NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_USERNAME,
 }
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import app from './app'
 import { Config } from './config'
 import logger from './config/logger'
 
+console.log(process.env.NODE_ENV)
+
 const startServer = () => {
     const { PORT } = Config
     try {
