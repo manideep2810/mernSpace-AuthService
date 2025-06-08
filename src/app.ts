dc4d176be9453b4f8757ea96443566bc9e651181
Manideep Naidu <manideepnaidugorle@gmail.com>
2025-06-08 14:23:47 +0530

    Fix cors origin url 2

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,8 @@ const app = express()
 app.use(cookieParser())
 app.use(
     cors({
-        origin: ['http://localhost:5173'],
         credentials: true,
+        origin: ['http://localhost:5173'],
     }),
 )
 app.use(express.json())
